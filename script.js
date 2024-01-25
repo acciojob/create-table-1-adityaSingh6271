e6426b06f13df461cfa7f28593dbe9e6aacbfe22
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-25 11:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function insert_Row() {
     // Get the table by its id
-    var table = document.getElementById("sampleTable");
+    const table = document.getElementById("sampleTable");
 
     // Insert a new row at the top of the table
-    var newRow = table.insertRow(0);
+    const newRow = table.insertRow(0);
 
     // Insert cells into the new row
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
 
     // Set the content of the cells
     cell1.innerHTML = "New Cell1";
